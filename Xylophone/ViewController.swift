e3cb78acb3828c59cf5884db4f296931a8df8c2c
JonathasLopes <jon.athas@hotmail.com>
2022-09-19 22:56:07 -0300

    opacity in buttons added

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -26,6 +26,11 @@ class ViewController: UIViewController {
     
     @IBAction func KeyPressed(_ sender: UIButton) {
         playSound(sound: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
 }
 
